9cf75273da6d862ec03aa8469a3ae328e20478e6
Tiago <Tiagocaulam@gmail.com>
2025-09-10 02:11:00 -0300

    Comecei a fazer o site

---
+++ src/components/data/products.js
@@ -0,0 +1,23 @@
+export const products = [
+  {
+    id: 1,
+    name: "Produto Incrível 1",
+    description: "Uma descrição breve e vendedora sobre os benefícios deste produto maravilhoso.",
+    image: "/images/produto-1.jpg", // Caminho para a imagem na pasta `public/images`
+    link: "https://shopee.com.br/SEU_LINK_PARA_O_PRODUTO_1" // Link direto para a Shopee
+  },
+  {
+    id: 2,
+    name: "Produto Fantástico 2",
+    description: "Ideal para resolver o problema X, com a qualidade e design que você procura.",
+    image: "/images/produto-2.jpg",
+    link: "https://shopee.com.br/SEU_LINK_PARA_O_PRODUTO_2"
+  },
+  {
+    id: 3,
+    name: "Produto Essencial 3",
+    description: "Não pode faltar no seu dia a dia. Garante mais praticidade e estilo.",
+    image: "/images/produto-3.jpg",
+    link: "https://shopee.com.br/SEU_LINK_PARA_O_PRODUTO_3"
+  }
+];
