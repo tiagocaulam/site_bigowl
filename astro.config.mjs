8101680c61e8ffea20613d142148fb08f3e28662
Tiago <Tiagocaulam@gmail.com>
2025-09-12 00:53:10 -0300

    teste

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,6 @@
 import { defineConfig } from 'astro/config';
 
 export default defineConfig({
-  // O 'site' agora é o seu domínio personalizado
-  site: 'https://www.bigowltcgstore.com.br',
-
-  // A propriedade 'base' foi REMOVIDA
+  site: 'https://tiagocaulam.github.io',
+  base: '/site_bigowl/',
 });
