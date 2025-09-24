342ad44fa164f540b7a09d10f02f1150194819ce
Tiago <Tiagocaulam@gmail.com>
2025-09-24 18:38:39 -0300

    Novos Produtos

--- src/components/data/products.js
+++ src/components/data/products.js
@@ -24,4 +24,85 @@ export const products = [
     link: "https://shopee.com.br/bigowl_tcgstore#product_list",
     estoque: 0
   },
+  {
+    id: 3,
+    name: "Booster Box Mega evoluções 36 pcts.",
+    description: "A caixa perfeita para colecionadores e jogadores. Contém 36 pacotes da coleção Mega Evoluções.",
+    image: "images/ME01-BoosterBox.jpg",
+    price: "-",
+    link: "#",
+    estoque: -1 // -1 indica 'em breve'
+  },
+  {
+    id: 5,
+    name: "DECK DE BATALHA POKEMON HOUNDOOM EX",
+    description: "Deck de Batalha Pokémon Houndoom EX. Pronto para jogar!",
+    image: "images/BB-Houndoom.jpg",
+    price: "-",
+    link: "#",
+    estoque: -1
+  },
+  {
+    id: 6,
+    name: "BLISTER TRIPLO POKEMON ME01 DRIFLOON",
+    description: "Blister Triplo Pokémon ME01 Drifloon. Três boosters para aumentar sua coleção.",
+    image: "images/ME01-Triplepack-Drifloon.jpg",
+    price: "-",
+    link: "#",
+    estoque: -1
+  },
+  {
+    id: 7,
+    name: "BLISTER TRIPLO POKEMON EV 10.5 RAIO PRETO REUNICLUS",
+    description: "Blister Triplo Pokémon EV 10.5 Raio Preto Reuniclus. Três boosters exclusivos.",
+    image: "images/RP-Triplepack.jpg",
+    price: "-",
+    link: "#",
+    estoque: -1
+  },
+  {
+    id: 8,
+    name: "BLISTER TRIPLO POKEMON ME01 DRIFBLIM",
+    description: "Blister Triplo Pokémon ME01 Drifblim. Três boosters para sua coleção.",
+    image: "images/ME01-Triplepack-Drifblim.png",
+    price: "-",
+    link: "#",
+    estoque: -1
+  },
+  {
+    id: 9,
+    name: "BLISTER UNITARIO POKEMON ME01",
+    description: "Blister Unitário Pokémon ME01. Um booster para completar sua coleção.",
+    image: "images/ME01-Blisterunitario.jpg",
+    price: "-",
+    link: "#",
+    estoque: -1
+  },
+  {
+    id: 10,
+    name: "BLISTER QUADRUPLO POKEMON ME01 GOLDUCK",
+    description: "Blister Quádruplo Pokémon ME01 Golduck. Quatro boosters e carta promocional.",
+    image: "images/ME01-Quadripack-Golduck.png",
+    price: "-",
+    link: "#",
+    estoque: -1
+  },
+  {
+    id: 11,
+    name: "BLISTER QUADRUPLO POKEMON ME01 PSYDUCK",
+    description: "Blister Quádruplo Pokémon ME01 Psyduck. Quatro boosters e carta promocional.",
+    image: "images/ME01-Quadripack-Psyduck.png",
+    price: "-",
+    link: "#",
+    estoque: -1
+  },
+  {
+    id: 13,
+    name: "BOX POKEMON COLEÇÃO PREMIUM EV8.5 EVOLUÇÕES PRISMÁTICAS",
+    description: "Box Pokémon Coleção Premium EV8.5 Evoluções Prismáticas. Produto especial para colecionadores.",
+    image: "images/EP-BoxPremium.jpg",
+    price: "-",
+    link: "#",
+    estoque: -1
+  }
 ];
